72e2ebf9fdeb942ff458c25843e881526c6357a5
hypermonkey <jwbrookes4@gmail.com>
2014-09-26 19:24:20 +0100

    Setup Project

---
+++ KSPRemoteLaunch/KSPRemoteLaunch/MonoBehaviourExtended.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace KSPRemoteLaunch
+{
+    ///<Summary>
+    /// Extended version of MonoBehavoiur
+    /// Handles logging to the console
+    ///</Summary>
+    public class MonoBehaviourExtended : MonoBehaviour
+    {
+        [System.Diagnostics.Conditional("DEBUG")]
+        internal static void LogDebugOnly(String Message, params object[] strParams)
+        {
+            Log(Message, strParams);
+        }
+
+        internal static void Log(String Message, params object[] strParams)
+        {
+
+            Debug.Log(String.Format(Message, strParams));
+        }
+
+        public static void h ()
+        {
+            
+        }
+    }
+}
---
+++ KSPRemoteLaunch/KSPRemoteLaunch/RemoteLaunchGUI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KSPRemoteLaunch
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class RemoteLaunchGUI:MonoBehaviourExtended
+    {
+
+    }
+}
