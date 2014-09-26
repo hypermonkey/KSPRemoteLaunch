380709097dc737195eeb8e9b6a0e159c6abeea90
hypermonkey <jwbrookes4@gmail.com>
2014-09-26 21:07:02 +0100

    Added window with 2 text fields to allow users to enter latitude and longitude. Added button to validate text (numbers only).

--- KSPRemoteLaunch/KSPRemoteLaunch/RemoteLaunchGUI.cs
+++ KSPRemoteLaunch/KSPRemoteLaunch/RemoteLaunchGUI.cs
@@ -9,6 +9,93 @@ namespace KSPRemoteLaunch
     [KSPAddon(KSPAddon.Startup.Flight, false)]
     public class RemoteLaunchGUI:MonoBehaviourExtended
     {
+        //windowPos is used to position the GUI window, lets set it in the center of the screen
+        protected Rect windowPos = new Rect(Screen.width / 2, Screen.height / 2, 10, 10);
+        private string latText = "0.0";
+        private string lonText = "0.0";
+        private double lat = 0;
+        private double lon = 0;
+        private bool windowActive = false;
 
+        private void WindowGUI(int windowID)
+        {
+            GUIStyle buttonSty = new GUIStyle(GUI.skin.button);
+            buttonSty.normal.textColor = buttonSty.focused.textColor = Color.white;
+            buttonSty.hover.textColor = buttonSty.active.textColor = Color.yellow;
+            buttonSty.onNormal.textColor = buttonSty.onFocused.textColor = buttonSty.onHover.textColor = buttonSty.onActive.textColor = Color.green;
+            buttonSty.padding = new RectOffset(8, 8, 8, 8);
+
+            GUIStyle textSty = new GUIStyle(GUI.skin.textField);
+            textSty.normal.textColor = buttonSty.focused.textColor = Color.white;
+            textSty.padding = new RectOffset(8, 8, 8, 8);
+
+            GUIStyle labelSty = new GUIStyle(GUI.skin.label);
+            labelSty.normal.textColor = buttonSty.focused.textColor = Color.white;
+            labelSty.padding = new RectOffset(8, 8, 8, 8);
+
+            GUILayout.BeginVertical();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Latitude:  ", labelSty, GUILayout.ExpandWidth(true));
+            latText = GUILayout.TextField(latText, textSty, GUILayout.ExpandWidth(true));
+            GUILayout.EndHorizontal();
+            
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Longitude: ", labelSty, GUILayout.ExpandWidth(true));
+            lonText = GUILayout.TextField(lonText, textSty, GUILayout.ExpandWidth(true));
+            GUILayout.EndHorizontal();
+            
+            if (GUILayout.Button("Log Position", buttonSty, GUILayout.ExpandWidth(true)))
+            {
+                try
+                {
+                    lat = double.Parse(latText);
+                    lon = double.Parse(lonText);
+                    LogDebugOnly("Latitude: {0} , Longitude: {1}", lat, lon);
+                }
+                catch(Exception e)
+                {
+                    LogDebugOnly(e.Message);
+                }
+            }
+
+
+            GUILayout.EndVertical();
+
+            GUI.DragWindow(new Rect(0, 0, 250, 30));
+        }
+
+
+        private void drawGUI()
+        {
+            GUI.skin = HighLogic.Skin;
+            windowPos = GUILayout.Window(1, windowPos, WindowGUI, "Remote Launch", GUILayout.MinWidth(250), GUILayout.MinHeight(150));
+
+        }
+
+        void Start()
+        {
+
+            RenderingManager.AddToPostDrawQueue(3, new Callback(drawGUI));//start the GUI
+
+            if ((windowPos.x == 0) && (windowPos.y == 0))//windowPos is used to position the GUI window, lets set it in the center of the screen
+            {
+                windowPos = new Rect(Screen.width / 2, Screen.height / 2, 10, 10);
+            }
+            windowActive = true;
+           
+            LogDebugOnly("GUI Added");
+
+
+        }
+
+
+
+
+        void onDestroy()
+        {
+            RenderingManager.RemoveFromPostDrawQueue(3, new Callback(drawGUI)); //close the GUI
+
+        }
     }
 }
